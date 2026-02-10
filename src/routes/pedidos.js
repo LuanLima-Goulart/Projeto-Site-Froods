c8770dd4c5b3a84da9d2e72d5d1bf58f5cd4944a
LUAN ALISON CAMARGO DE LIMA <luan_a_lima@estudante.sesisenai.org.br>
2026-02-10 20:53:45 -0300

    Delete, Post e Put

--- src/routes/pedidos.js
+++ src/routes/pedidos.js
@@ -12,4 +12,23 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.delete('/:id/permanente', async (req, res) => {
+    const clientesId = req.params.id;
+
+    try {
+        const [clientes] = await pool.execute('SELECT * FROM pedidos WHERE idPedido = ?', [clientesId]);
+        if (clientes.length === 0) {
+            return res.status(404).json({ erro: 'Pedido não encontrado' });
+        }
+
+        const [pedidos] = await pool.execute('SELECT COUNT(*) as total FROM pedidos WHERE idPedido = ?', [clientesId]);
+
+        await pool.execute('DELETE FROM pedidos WHERE idPedido = ?', [clientesId]);
+        res.json({ mensagem: 'Pedido removido do sistema com sucesso' });
+
+    } catch (error) {
+        res.status(500).json({ erro: 'Erro ao excluir', detalhes: error.message });
+    }
+});
+
 module.exports = router;
